56f9e0184590b0433c4e0fed5b9c59fd8ab22566
Shubham Katara <shubham.katara@trivago.com>
2024-01-15 14:42:22 +0100

    change: code for halding posts not found during CRUD  operations

--- app.py
+++ app.py
@@ -49,54 +49,73 @@ async def users():
 
 @app.get("/post/{passed_id}", status_code=status.HTTP_200_OK)
 async def post_by_id(passed_id: int, status_code: Response):
-    post = find_post_by_id(passed_id)
-    if not post:
+    find_index = find_post_by_id(passed_id)
+
+    if find_index == None:
         status_code.status_code = status.HTTP_404_NOT_FOUND
         return {
             "msg": "Not Found",
             "status_code": status.HTTP_404_NOT_FOUND
         }
-    return post[0]
+    return find_index[0]
 
 @app.post("/createpost",status_code=status.HTTP_201_CREATED)
 async def create_item(post: post_schema):
     post_dictionary = post.model_dump()
     post_dictionary['id'] = randrange(0,1000000)
     content.append(post_dictionary)
-    return {"msg": "data added successfully"}
+    return {
+        "msg": "data added successfully"
+        }
 
 @app.delete("/delete/post/{passed_id}")
-def delete_post(passed_id: int):
-    post = find_post_by_id(passed_id)
-    if not post:
+def delete_post(passed_id: int,status_code: Response):
+    find_index = find_post_by_id(passed_id)
+
+    if find_index == None:
+        status_code.status_code = status.HTTP_404_NOT_FOUND
         return {
             "msg": "post not found",
-            "status_code": status.HTTP_404_NOT_FOUND
+            "status_code": status_code.status_code
         }
-    content.remove(post)
-    return content
-
+    else:
+        content.remove(find_index[0])
+        return content
+    
 @app.put("/update/{passed_id}",status_code=status.HTTP_200_OK)
 def update(post: post_schema, passed_id: int):
     find_index = find_post_by_id(passed_id)
-    post_index = find_index[1]
-    content[post_index] = post.model_dump()
-    content[post_index]['id'] = passed_id
-    return content
+
+    if find_index == None:
+        return {
+            "msg": "Post not found"
+        }
+        
+    else:
+        post_index = find_index[1]
+        content[post_index] = post.model_dump()
+        content[post_index]['id'] = passed_id
+        return content[post_index]
 
 @app.patch("/patch_post_title/{passed_id}",status_code=status.HTTP_200_OK)
 async def patch(passed_id: int, request_patch: Request):
     find_index = find_post_by_id(passed_id)
-    post_index = find_index[1]
     request_data = await request_patch.json()
-    for k,v in request_data.items():
-        if k == "title":
-            content[post_index]['title'] =  request_data['title']
-        if k == "age":
-            content[post_index]['age'] =  request_data['age']
-        if k == "testingdata":
-            content[post_index]['testingdata'] =  request_data['testingdata']
-    return content[post_index]
+
+    if find_index == None:
+        status_code = status.HTTP_404_NOT_FOUND
+        return {"msg":"Post not found"}
+    else:
+        post_index = find_index[1]
+        for k,v in request_data.items():
+            if k == "title":
+                content[post_index]['title'] =  request_data['title']
+            if k == "age":
+                content[post_index]['age'] =  request_data['age']
+            if k == "testingdata":
+                content[post_index]['testingdata'] =  request_data['testingdata']
+        return content[post_index]
+        
 
 if __name__ == "__main__":
     find_post_by_id(1)
