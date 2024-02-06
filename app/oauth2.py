729f58c7f5414dee1686f3e9588189be2dfd4a4c
Shubham Katara <shubham.katara@trivago.com>
2024-02-06 19:32:35 +0100

    Invalidate JWT Token if expired

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
 from jose import jwt,JWTError
+from time import time
 from fastapi import Header
 
 #This code returns a JWT token upon user login.
@@ -10,21 +11,21 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 def create_access_token(payload: dict):
     payload_encode = payload.copy()
     expire_time = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    print(expire_time)
     #create token expiry time ( token is valid for 30m from now )
     payload_encode.update({"exp": expire_time})
 
     #generate JWT token
     return jwt.encode(payload_encode,SECRET_KEY,algorithm=ALGORITHM)
 
-def return_token_info(header: str = Header(default=None)):
-    return {"token": header}
-
 def validate_access_token(token: str):
     try:
         payload = jwt.decode(token,SECRET_KEY,ALGORITHM)
         user_email = payload.get('email')
+        token_expire = payload.get("exp")
         if user_email is not None:
-            return {"email": user_email}   
+            return {"email": user_email,"expire": False} 
     except:
-        return {"msg":"Unauthenticated","detail":"Could not Validate Authentication. Please login again"}
+        return {"expire": True}
+        
     
--- app/routers/posts.py
+++ app/routers/posts.py
@@ -14,62 +14,60 @@ posts_router = APIRouter(
 @posts_router.get("/list")
 def list_posts(Authorization: str = Header(default=None)):
     token = Authorization
-    if "email" in validate_access_token(token):
+    if validate_access_token(token)['expire']:
+        return {"Error": "Token Invalid."}
+    else:
         result_list = []
         select_sql_instruction = Select(posts_table.c.title,posts_table.c.content, posts_table.c.firstname, posts_table.c.lastname)
         exec_sql = conn.execute(select_sql_instruction).all()
         for row in exec_sql: 
             result_list = result_list + [list(row)]
         return result_list
-    else:
-        return {"msg":"Unauthenticated","detail":"Could not Validate Authentication. Please login again"}
-
+    
 
 @posts_router.get("/fetch/{passed_id}", status_code=status.HTTP_200_OK)
 async def post_by_id(passed_id: int, status_code: Response,Authorization: str = Header(default=None)):
     token = Authorization
-    if "email" in validate_access_token(token):
+    if validate_access_token(token)['expire']:
+        return {"Error": "Token Invalid."}
+    else:
         post = find_post_in_db(passed_id)
         return post
-    else:
-        return {"msg":"Unauthenticated","detail":"Could not Validate Authentication. Please login again"}
+
     
 @posts_router.post("/create",status_code=status.HTTP_201_CREATED)
 def create_item(post: post_schema, Authorization: str = Header(default=None)):
     token = Authorization
-    if "email" in validate_access_token(token):
+    if validate_access_token(token)['expire']:
+        return {"Error": "Token Invalid."}
+    else:
         insert_sql_statement = posts_table.insert().values(title=post.title,firstname=post.firstname,lastname=post.lastname,content=post.content)
         conn.execute(insert_sql_statement)
         return {"msg": "Data inserted successfully"} if conn.commit() is None else {"msg": "Data not inserted successfully"}
-    else:
-        return {"msg":"Unauthenticated","detail":"Could not Validate Authentication. Please login again"}
 
 
 @posts_router.delete("/delete/{passed_id}")
 def delete_post(passed_id: int,status_code: Response,Authorization: str = Header(default=None)):
     token = Authorization
-    if "email" in validate_access_token(token):
+    if validate_access_token(token)['expire']:
+        return {"Error": "Token Invalid."}
+    else:
         findPost = find_post_in_db(passed_id)
         if isinstance(findPost,dict):
             return {"msg":"Post Not Found"}
         delete_sql_instruction = posts_table.delete().where(posts_table.c.id == passed_id)
         conn.execute(delete_sql_instruction)
         return {"msg": "Post Succesfully deleted"} if conn.commit() is None else {"msg": "Post Not deleted"}
-    else:
-        return {"msg":"Unauthenticated","detail":"Could not Validate Authentication. Please login again"}
-   
-
     
 @posts_router.put("/update/{passed_id}",status_code=status.HTTP_200_OK)
 def update(post: post_schema, passed_id: int,Authorization: str = Header(default=None)):
     token = Authorization
-    if "email" in validate_access_token(token):
+    if validate_access_token(token)['expire']:
+        return {"Error": "Token Invalid."}
+    else:
         findPost = find_post_in_db(passed_id)
         if isinstance(findPost,dict):
             return {"msg":"Post Not Found"}
         update_post_sql = posts_table.update().where(posts_table.c.id == passed_id).values(title=post.title,firstname=post.firstname,lastname=post.lastname,content=post.content)
         conn.execute(update_post_sql)
         return {"msg": "Post Succesfully Updated"} if conn.commit() is None else {"msg": "Post Not updated"}
-    else:
-        return {"msg":"Unauthenticated","detail":"Could not Validate Authentication. Please login again"}
-
