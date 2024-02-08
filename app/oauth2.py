1845b2910ca12048586784e323104bed35ae134c
Shubham Katara <shubham.katara@trivago.com>
2024-02-08 15:47:58 +0100

    validate jwt payload email id against database email id

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,7 +1,6 @@
 from datetime import datetime, timedelta
-from jose import jwt,JWTError
-from time import time
-from fastapi import Header
+from jose import jwt
+from database import find_user_in_db
 
 #This code returns a JWT token upon user login.
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
@@ -20,10 +19,13 @@ def create_access_token(payload: dict):
 def validate_access_token(token: str):
     try:
         payload = jwt.decode(token,SECRET_KEY,ALGORITHM)
-        user_email = payload.get('email')
+        user_email_in_jwt = payload.get('email')
+        user_email_in_database = find_user_in_db(user_email_in_jwt)[0]
         token_expire = payload.get("exp")
-        if user_email is not None:
-            return {"email": user_email,"expire": False,"msg":f'Welcome {user_email}'} 
+        if user_email_in_jwt is not None:
+            return {"email": user_email_in_jwt,"expire": False,"msg":f'Welcome {user_email_in_jwt}'} if user_email_in_jwt == user_email_in_database else {"email": user_email_in_jwt,"expire": False,"msg":f'Invalid User Email {user_email_in_jwt}'}
+                
+        
     except:
         return {"expire": True,"msg": "Token Expired. Please Login Again."}
         
