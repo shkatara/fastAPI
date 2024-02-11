5bcc41cbce015e928c97a67dbb436e0e6944a04d
Shubham Katara <shubham.katara@trivago.com>
2024-02-11 17:12:10 +0100

    Use Database values from environment variable instead of hard code

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,10 +1,13 @@
 from datetime import datetime, timedelta
 from jose import jwt
 from database import find_user_in_db
+from os import getenv
+from dotenv import load_dotenv
 
+load_dotenv()
 #This code returns a JWT token upon user login.
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
+SECRET_KEY = getenv("SECRET_KEY")
+ALGORITHM = getenv("ALGORITHM")
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 def create_access_token(payload: dict):
